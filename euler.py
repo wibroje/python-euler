30e71313b8d73be45a561d0ec7e57e1509fd117a
William Jefferies <willjefferies10@gmail.com>
2018-05-16 09:49:33 -0600

    This is hard

--- euler.py
+++ euler.py
@@ -19,22 +19,41 @@
 # 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-sum = 0
-a = 1
-b = 0
-while a < 4000000:
-	a = b + a
-	b = a - b
-	if a % 2 == 0:
-		sum = sum + a
+# sum = 0
+# a = 1
+# b = 0
+# while a < 4000000:
+# 	a = b + a
+# 	b = a - b
+# 	if a % 2 == 0:
+# 		sum = sum + a
 
-print(sum)
+# print(sum)
+
+# The sum is 4613732
+
 
 # 3.
 
 # The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143?
 
-# What is the largest prime factor of the number 600851475143 ?
+
+# 4. 
+
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+# 5. 
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+	
+
+
 
 
 
